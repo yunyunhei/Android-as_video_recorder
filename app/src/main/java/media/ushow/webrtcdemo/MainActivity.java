d08a93794426be8b826ab087960fe86ff48e67f1
wuhang <1131029442@qq.com>
2019-10-28 09:08:55 +0800

    update .gitignore

--- app/src/main/java/media/ushow/webrtcdemo/MainActivity.java
+++ app/src/main/java/media/ushow/webrtcdemo/MainActivity.java
@@ -1,7 +1,11 @@
 package media.ushow.webrtcdemo;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -32,6 +36,27 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+
+        int selfPermission = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        if (selfPermission != PackageManager.PERMISSION_GRANTED){
+            String[] permissions = new String[3];
+            permissions[0] = Manifest.permission.CAMERA;
+            permissions[1] = Manifest.permission.RECORD_AUDIO;
+            permissions[2] = Manifest.permission.WRITE_EXTERNAL_STORAGE;
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                this.requestPermissions(permissions,101);
+            }
+        }
+
+
     }
 
     /**
